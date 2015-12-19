d1c4542d3723f8150e7ea6f310a495c5f2c56cc1
ranjiterao <ranjiterao@gmail.com>
2015-12-18 22:11:38 -0800

    Create schema and work on tests

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+/* Describe your table here.*/
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT, 
+  username VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 );
+
+-- CREATE TABLE room (
+--   id INT AUTO_INCREMENT,
+--   roomName VARCHAR(255) NOT NULL, 
+--   PRIMARY KEY (id)
+-- );
+
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT, 
+  message VARCHAR(255) NOT NULL,
+  user_id INT NOT NULL,
+  room VARCHAR (255) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  -- FOREIGN KEY (room) REFERENCES room(id)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
